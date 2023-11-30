0c9167f793c37ec60b93afc9a69cc2db35911a7e
daniyal392 <134169837+daniyal392@users.noreply.github.com>
2023-11-30 05:56:39 +0200

    implemented Unit Testing

--- LocalTesting 2/app/src/test/java/jlin2/examples/localtesting/ExampleUnitTest.kt
+++ LocalTesting 2/app/src/test/java/jlin2/examples/localtesting/ExampleUnitTest.kt
@@ -14,4 +14,21 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun checkIfEmailIsValid() {
+        assertTrue(EmailValidator.isValidEmail("123@abc.com"))
+        assertTrue(EmailValidator.isValidEmail("123@abc.co.ca"))
+    }
+
+    @Test
+    fun checkIfEmailIsInvalid() {
+        assertFalse(EmailValidator.isValidEmail("123@abc"))
+        assertFalse(EmailValidator.isValidEmail("123@abc..com"))
+        assertFalse(EmailValidator.isValidEmail("@abc.com"))
+        assertFalse(EmailValidator.isValidEmail("testing123"))
+        assertFalse(EmailValidator.isValidEmail(""))
+        assertFalse(EmailValidator.isValidEmail(null))
+    }
+
 }
